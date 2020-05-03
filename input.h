ba93961a86f2974a4e49211afb8042ea3c1d3d3f
Dominic Bertocchini <dominicbertocchini@gmail.com>
2020-05-03 11:00:09 -0700

    added input files and class

---
+++ input.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "vcpkg/installed/x86-windows/include/SDL2/SDL.h"
+
+class Input
+{
+public:
+	//SDL event handler
+	SDL_Event e;
+
+	//if mouse was pressed
+	bool leftClick, rightClick;
+	//if mouse is held
+	bool leftButton, rightButton;
+
+	Input();
+
+	//check if a specific key is down
+	static bool checkKey(const char* key);
+	//get mouse position
+	static SDL_Point getPos();
+	//update based on inputs
+	void update(bool quit);
+};
